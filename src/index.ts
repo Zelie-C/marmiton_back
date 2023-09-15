7773fc06c248254d081461bdec26b6936115dc8b
Zelie-C <zelie.chemin@gmail.com>
2023-09-15 15:46:28 +0200

    routes fonctionnelles + delete

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
-import express from 'express'
+import express, { Request } from 'express'
 import 'dotenv/config'
 import { Sequelize, DataTypes } from "sequelize";
-
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 
 let app = express();
+app.use(cors());
+app.use(bodyParser.json());
 const port = parseInt(process.env.PORT as string);
 
 
 const username = process.env.USERNAME as string;
 const password = process.env.PASSWORD as string;
-const database = process.env.PASSWORD as string;
-const server= process.env.SERVE as string;
-const connexion= process.env.CONNECTION_STRING as string
+const database = process.env.DATABASE as string;
+const server= process.env.SERVER as string;
 
 
-const sequelize = new Sequelize(connexion)
+const sequelize = new Sequelize(database, username, password, {
+  host: server,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true,
+  }
+});
 
 
 async function connexionTest() {
@@ -51,37 +59,41 @@ const Recette = sequelize.define('Recette', {
 });
 
 Recette.sync()
+// Recette.sync({force: true})
 
-// app.get('/', async (_, res) => {
-//   res.send('OK')
-// })
 
-app.post('/recipe/create/:name/:image/:duration/:note', async (req, res) => {
-  let name = req.params.name;
-  let image = req.params.image;
-  let duration = req.params.duration;
-  let note = req.params.note;
-  await Recette.create({
+interface IRequestBody {
+  name: string,
+  image: string,
+  duration: string,
+  note: string,
+}
+
+app.post('/recipes', async (req: Request<IRequestBody>, res) => {
+  let name = req.body.name;
+  let image = req.body.image;
+  let duration = req.body.duration;
+  let note = req.body.note;
+  const maRecette = await Recette.create({
     name: name,
     urlimage: image,
     duration: duration,
     note: note
   })
-  res.send('ok')
+  res.json(maRecette)
 })
 
-app.delete('/recipe/delete/:id', async (req, res) => {
+app.delete('/recipes/:id', async (req, res) => {
   let idToDelete = req.params.id;
   await Recette.destroy({
     where: {
       id: idToDelete
     }
   });
-
   res.send('deleted')
 })
 
-app.get('/recipe/getallrecipes', async (_, res) => {
+app.get('/recipes', async (_, res) => {
   const allRecipes = await Recette.findAll();
   res.send(allRecipes)
 })
